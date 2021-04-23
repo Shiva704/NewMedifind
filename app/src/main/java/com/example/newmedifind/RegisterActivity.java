804623fab35108001e4728fe3ec340180692dfbf
Shiv704 <shivakumarmamadapur12@gmail.com>
2021-04-23 14:35:22 +0530

    Now you can Register

--- app/src/main/java/com/example/newmedifind/RegisterActivity.java
+++ app/src/main/java/com/example/newmedifind/RegisterActivity.java
@@ -27,14 +27,135 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         intia();
+        Listners();
     }
+    private Boolean validateName() {
+        String val= Username.getEditText().getText().toString();
+        String noWhiteSpace = "\\A\\w{4,20}\\z";
+
+        if(val.isEmpty()){
+            Username.setError("Field cannot be Empty");
+            return false;
+        }
+        else if(val.length()>=15){
+            Username.setError("Username too Long");
+            return false;
+        }
+        else if(!val.matches(noWhiteSpace)){
+            Username.setError("White space not allowed");
+            return false;
+        }
+        else{
+            Username.setError(null);
+            Username.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private Boolean validatePhonenumber() {
+        String val=Phonenumber.getEditText().getText().toString();
+
+        if(val.isEmpty()){
+            Phonenumber.setError("Field cannot be Empty");
+            return false;
+        }
+        else{
+            Phonenumber.setError(null);
+            Phonenumber.setErrorEnabled(false);
+            return true;
+        }
+    }
+    private Boolean validateEmail(){
+        String val=Email.getEditText().getText().toString();
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if(val.isEmpty()){
+            Email.setError("Field cannot be Empty");
+            return false;
+        }
+        else if(!val.matches(emailPattern)){
+            Email.setError("Inavlid Email type");
+            return false;
+        }
+        else{
+            Email.setError(null);
+            Email.setErrorEnabled(false);
+            return true;
+        }
+    }
+    private Boolean validatePassword(){
+        String val=Password.getEditText().getText().toString();
+        String passwordVal = "^" +
+                //"(?=.*[0-9])" +         //at least 1 digit
+                //"(?=.*[a-z])" +         //at least 1 lower case letter
+                //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +      //any letter
+                "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                "(?=\\S+$)" +           //no white spaces
+                ".{4,}" +               //at least 4 characters
+                "$";
+
+
+        if(val.isEmpty()){
+            Password.setError("Field cannot be Empty");
+            return false;
+        }
+        else if (!val.matches(passwordVal)) {
+            Password.setError("Password is too weak");
+            return false;
+        }
+        else{
+            Password.setError(null);
+            Password.setErrorEnabled(false);
+            return true;
+        }
+    }
+    private Boolean validateConfirmPassword(){
+        String val=Confirmpassword.getEditText().getText().toString();
+        String passwordVal = "^" +
+                //"(?=.*[0-9])" +         //at least 1 digit
+                //"(?=.*[a-z])" +         //at least 1 lower case letter
+                //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +      //any letter
+                "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                "(?=\\S+$)" +           //no white spaces
+                ".{4,}" +               //at least 4 characters
+                "$";
+
+
+        if(val.isEmpty()){
+            Confirmpassword.setError("Field cannot be Empty");
+            return false;
+        }
+        else if (!val.matches(passwordVal)) {
+            Confirmpassword.setError("Password is too weak");
+            return false;
+        }
+        else{
+            Confirmpassword.setError(null);
+            Confirmpassword.setErrorEnabled(false);
+            return true;
+        }
+    }
+
     private void Listners(){
 
         Loginback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 rootnode=FirebaseDatabase.getInstance();
-                reference=rootnode.getReference("Coustmers");
+                reference= rootnode.getReference("Coustmers");
+
+                Boolean validateName = validateName();
+                Boolean validatePhonenumber= validatePhonenumber();
+                Boolean validateEmail = validateEmail();
+                Boolean validatePassword = validatePassword();
+                Boolean validateConfirmPassword = validateConfirmPassword();
+
+
+                if( !validateName | !validatePhonenumber | !validateEmail | !validatePassword | !validateConfirmPassword){
+                    return;
+                }
 
 
                 String username= Username.getEditText().getText().toString();
@@ -43,22 +164,27 @@ public class RegisterActivity extends AppCompatActivity {
                 String password= Password.getEditText().getText().toString();
                 String confirmpassword= Confirmpassword.getEditText().getText().toString();
 
-                UserHelperClass helperClass= new UserHelperClass( username,phonenumber,email,password,confirmpassword);
 
+
+                UserHelperClass helperClass= new UserHelperClass( username,phonenumber,email,password,confirmpassword);
                 reference.child(username).setValue(helperClass);
+
             }
         });
 
-
     }
+
     private void intia(){
       Logoimage=findViewById(R.id.LogoImage);
       Slogan=findViewById(R.id.Slogon);
-     Username=findViewById(R.id.username);
+      Username=findViewById(R.id.username);
       Phonenumber=findViewById(R.id.phonenumber);
       Email=findViewById(R.id.email);
       Password=findViewById(R.id.password);
       Confirmpassword=findViewById(R.id.confirmpassword);
       Loginback=findViewById(R.id.Loginback);
     }
+
+
+
 }
