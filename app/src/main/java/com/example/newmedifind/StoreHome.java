fd719347dfe64d82a3665bca02b38d4b650bf4be
prajwal-annigeri <prajwal.annigeri@gmail.com>
2021-04-28 20:03:21 +0530

    Simple Store Home added

--- app/src/main/java/com/example/newmedifind/StoreHome.java
+++ app/src/main/java/com/example/newmedifind/StoreHome.java
@@ -5,20 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class StoreHome extends AppCompatActivity {
 
-    TextView textView;
+    Button Add_medicine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_home);
 
-        textView= findViewById(R.id.textView);
+        Add_medicine= findViewById(R.id.Add_Medicine);
 
-        textView.setOnClickListener(new View.OnClickListener() {
+        Add_medicine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(StoreHome.this,AddMedicine.class);
