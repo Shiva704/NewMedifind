fe63fb4cf3bbf09d7d2980c2a112c1f91a653c65
Shiv704 <shivakumarmamadapur12@gmail.com>
2021-04-30 16:34:10 +0530

    onClick

--- app/src/main/java/com/example/newmedifind/StoreHome.java
+++ app/src/main/java/com/example/newmedifind/StoreHome.java
@@ -1,5 +1,6 @@
 package com.example.newmedifind;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,9 +9,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class StoreHome extends AppCompatActivity {
 
     Button Add_medicine;
+    TextView Display;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,7 @@ public class StoreHome extends AppCompatActivity {
         setContentView(R.layout.activity_store_home);
 
         Add_medicine= findViewById(R.id.Add_Medicine);
+        Display= findViewById(R.id.welcome);
 
         Add_medicine.setOnClickListener(new View.OnClickListener() {
             @Override
